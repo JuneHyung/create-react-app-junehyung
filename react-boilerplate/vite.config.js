ac12c4924a24414f714ae401ab9bae66bce59702
JUNEHYUNG\USER <cjh951114@gmail.com>
2024-05-17 15:45:42 +0900

    example init

---
+++ react-boilerplate/vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
